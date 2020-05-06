00821cb016a778c52359341b031a14bc28542220
Patrik Márton <marpataa@yahoo.com>
2020-05-06 17:34:45 +0200

    Update UI

--- app/src/main/java/com/example/beerfinder/presenter/DetailsPresenter.java
+++ app/src/main/java/com/example/beerfinder/presenter/DetailsPresenter.java
@@ -1,9 +1,14 @@
 package com.example.beerfinder.presenter;
 
+import com.example.beerfinder.BeerFinderApplication;
 import com.example.beerfinder.db.Repository;
+import com.example.beerfinder.model.Beer;
 import com.example.beerfinder.network.NetworkInteractor;
 import com.example.beerfinder.view.IDetailsView;
 import com.example.beerfinder.view.IListView;
+
+import java.io.IOException;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -17,7 +22,30 @@ public class DetailsPresenter extends Presenter<IDetailsView>{
 
     @Inject
     public DetailsPresenter(){
+    }
 
+    @Override
+    public void attachScreen(IDetailsView screen) {
+        super.attachScreen(screen);
+        BeerFinderApplication.injector.inject(this);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+
+    public List<Beer> getBeers() throws IOException {
+        return networkInteractor.getBeers();
+    }
+
+    public Beer getBeer(List<Beer> beers, String name){
+        for(Beer beer: beers) {
+            if (beer.getName().equals(name)) {
+                return beer;
+            }
+        }
+        return beers.get(1);
     }
 
     //TODO
--- app/src/main/java/com/example/beerfinder/view/DetailsActivity.java
+++ app/src/main/java/com/example/beerfinder/view/DetailsActivity.java
@@ -2,13 +2,19 @@ package com.example.beerfinder.view;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.beerfinder.BeerFinderApplication;
 import com.example.beerfinder.R;
+import com.example.beerfinder.model.Beer;
 import com.example.beerfinder.presenter.DetailsPresenter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -19,27 +25,48 @@ public class DetailsActivity extends AppCompatActivity implements IDetailsView {
 
     String data;
 
+    List<Beer> beers;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BeerFinderApplication.injector.inject(this);
 
+        TextView tv1 = (TextView) findViewById(R.id.text_view1);
+        TextView tv2 = (TextView) findViewById(R.id.text_view2);
+        TextView tv3 = (TextView) findViewById(R.id.text_view3);
+
         Bundle extras = getIntent().getExtras();
         if(extras != null)
              data = extras.getString("name");
 
-        Log.d("TETETETETETTETETETE",data);
+        try {
+            beers = detailsPresenter.getBeers();
+        } catch (IOException e) {
+            e.printStackTrace();
+            beers = new ArrayList<>();
+        }
+
+        Beer b = detailsPresenter.getBeer(beers,data);
+
+        tv1.setText(b.getName());
+        tv2.setText(b.getDescription());
+        tv3.setText(b.getTagline());
+
+        //Log.d("TETETETETETTETETETE",data);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        detailsPresenter.attachScreen(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        detailsPresenter.detachScreen();
     }
 
     @Override
